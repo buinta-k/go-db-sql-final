ffdc0dac467c959a1434c903bc1656081eff0610
buinta-k <22255z2h72@privaterelay.appleid.com>
2026-06-26 22:35:48 +0300

    Update parcel.go

--- parcel.go
+++ parcel.go
@@ -13,55 +13,61 @@ func NewParcelStore(db *sql.DB) ParcelStore {
 }
 
 func (s ParcelStore) Add(p Parcel) (int, error) {
-	res, err:=s.db.Exec("INSERT INTO parcel (client, status, address, created_at) VALUES (:client, :status, address, :created_at)",
-					sql.Named("client", p.Client),
-					sql.Named("status", p.Status),
-					sql.Named("address", p.Address).
-					sql.Named("create_at", p.CreateAt))
-	if err!=nil {
+	res, err := s.db.Exec("INSERT INTO parcel (client, status, address, created_at) VALUES (:client, :status, :address, :created_at)",
+		sql.Named("client", p.Client),
+		sql.Named("status", p.Status),
+		sql.Named("address", p.Address),
+		sql.Named("created_at", p.CreatedAt),
+	)
+	if err != nil {
 		return 0, err
 	}
-	
-	id, err:= res.LastInsertId()
-	if err!=nil {
+
+	id, err := res.LastInsertId()
+	if err != nil {
 		return 0, err
 	}
-	
+
 	return int(id), nil
 }
 
 func (s ParcelStore) Get(number int) (Parcel, error) {
 	p := Parcel{}
 
-	row:=s.db.QueryRow(`SELECT number, client, status, address, created_at FROM parcel WHERE number=:number`,
-					  sql.Named("number", number))
+	row := s.db.QueryRow(`SELECT number, client, status, address, created_at FROM parcel WHERE number=:number`,
+		sql.Named("number", number))
 
-	err:=row.Scan(&p.Number, &p.Client, &p.Status, &p.Address, &p.CreatedAt)
-	if err!=nil {
+	err := row.Scan(&p.Number, &p.Client, &p.Status, &p.Address, &p.CreatedAt)
+	if err != nil {
 		return p, err
 	}
-	
+
 	return p, nil
 }
 
 func (s ParcelStore) GetByClient(client int) ([]Parcel, error) {
-	rows, err:=s.db.Query(`SELECT number, client, status address, created_at FROM parcel WHERE client = :client`,
-						 sql.Named("client", client))
-	if err!=nil {
+	rows, err := s.db.Query(`SELECT number, client, status, address, created_at FROM parcel WHERE client = :client`,
+		sql.Named("client", client))
+	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
-	
+
 	var res []Parcel
 
 	for rows.Next() {
-	p:=Parcel{}
-	err:=rows.Scan(&p.Number, &p.Client, &p.Status, &p.Address, &p.CreatedAt)
-	if err!=nil {
+		p := Parcel{}
+		err := rows.Scan(&p.Number, &p.Client, &p.Status, &p.Address, &p.CreatedAt)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, p)
+	}
+
+	if err = rows.Err(); err != nil {
 		return nil, err
 	}
-	res=append(res, p)
-	}
+
 	return res, nil
 }
 
@@ -70,19 +76,18 @@ func (s ParcelStore) SetStatus(number int, status string) error {
 		sql.Named("status", status),
 		sql.Named("number", number))
 	return err
-
 }
 
 func (s ParcelStore) SetAddress(number int, address string) error {
-	_, err:=s.db.Exec(`UPDATE parcel SET address = :address WHERE number = :number AND status = 'registered'`,
-					 sql.Named("address", address),
-					 sql.Named("number", number))
-
+	_, err := s.db.Exec(`UPDATE parcel SET address = :address WHERE number = :number AND status = 'registered'`,
+		sql.Named("address", address),
+		sql.Named("number", number))
 	return err
 }
 
 func (s ParcelStore) Delete(number int) error {
-		_, err := s.db.Exec(`DELETE FROM parcel WHERE number = :number AND status = 'registered'`,
+	_, err := s.db.Exec(`DELETE FROM parcel WHERE number = :number AND status = 'registered'`,
 		sql.Named("number", number))
 	return err
 }
+
